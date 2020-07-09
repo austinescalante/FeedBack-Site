b957bec239df06217578340ab2c6aec738f3e1be
austinescalante <60208327+austinescalante@users.noreply.github.com>
2020-07-09 14:51:34 -0700

    SQL and Route Verification

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
 from flask import Flask, render_template, request
-
+from flask_sqlalchemy import SQLAlchemy
 
 #Data entered and redirection of site
 
 #Initialize app
 app = Flask(__name__)
+
+ENV = 'dev'
+
+if ENV == 'dev':
+    app.debug = True
+    #Dev database and key
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:dprlive1@localhost/bmw'
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+#Create DB object and query database
+db = SQLAlchemy(app)
+
+#Create a model
+class Feedback(db.Model):
+    __tablename__ = 'feedback'
+    id = db.Column(db.Integer, primary_key=True)
+    customer = db.Column(db.String(200), unique=True)
+    dealer = db.Column(db.String(200))
+    rating = db.Column(db.Integer)
+    comments = db.Column(db.Text())
+
+    def __init__(self, customer, dealer, rating, comments):
+        self.customer = customer
+        self.dealer = dealer
+        self.rating = rating
+        self.comments = comments
+
 #Create a route
 @app.route('/')
 def index():
@@ -27,6 +59,5 @@ def submit():
 
 
 if __name__ == '__main__':
-    app.debug = True
     app.run()
 
